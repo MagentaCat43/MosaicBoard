e1c04baf9148dbbd0f00e96602cc4b45d0238142
MagentaCat43 <nf17476@gmail.com>
2025-09-29 19:59:36 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-// Destacar página ativa no menu
+// Destacar página ativa
 document.querySelectorAll("nav a").forEach(link => {
   if (link.href === window.location.href) {
     link.classList.add("ativo");
   }
 });
+
+// Mostrar / esconder sidebar
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  if(sidebar.style.transform === 'translateX(-220px)') {
+    sidebar.style.transform = 'translateX(0)';
+  } else {
+    sidebar.style.transform = 'translateX(-220px)';
+  }
+}
